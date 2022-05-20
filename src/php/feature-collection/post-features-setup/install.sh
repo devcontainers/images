b56aa34e6c3ee3666f42ed0cf70a7d2d982fe3c7
Samruddhi Khandale <skhandale@microsoft.com>
2022-05-20 18:29:08 +0000

    remove commented code

--- src/php/feature-collection/post-features-setup/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-ARG USERNAME=vscode
-
-apt-get -y install --no-install-recommends lynx
-usermod -aG www-data ${USERNAME}
-sed -i -e "s/Listen 80/Listen 80\\nListen 8080/g" /etc/apache2/ports.conf
-apt-get clean -y && rm -rf /var/lib/apt/lists/*
