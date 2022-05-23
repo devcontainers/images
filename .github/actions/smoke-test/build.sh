aae25ce061f9495e816543bb9d07a45e3b1af25d
Samruddhi Khandale <skhandale@microsoft.com>
2022-05-23 23:05:25 +0000

    debug 1

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -4,16 +4,13 @@ DEFINITION="$1"
 set -e
 
 export DOCKER_BUILDKIT=1
-
-# Symlink build scripts from main to improve security when testing PRs
-if [ -d "$GITHUB_WORKSPACE/__build/build" ]; then
-    cp -r "$GITHUB_WORKSPACE/__build/build" "$GITHUB_WORKSPACE/"
-else
-    echo "WARNING: Using build/vscdc from $GITHUB_REF instead of main."
-fi
-
+echo "Building image"
+dir=$(pwd)
+echo $pwd
+ls -l
 # Build the image
 chmod +x build/devcontainers-cli.tgz
+ls -l
 npx --yes build/devcontainers-cli.tgz build --workspace-folder src/${DEFINITION} --image-name vsc-${DEFINITION}
 
 docker images
