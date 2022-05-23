f35e7ce263c5486cfc0e6dfaed456143f0cdc891
Samruddhi Khandale <skhandale@microsoft.com>
2022-05-23 21:53:33 +0000

    remove comments

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -5,26 +5,8 @@ set -e
 
 export DOCKER_BUILDKIT=1
 
-# Symlink build scripts from main to improve security when testing PRs
-# if [ -d "$GITHUB_WORKSPACE/__build/build" ]; then
-#     cp -r "$GITHUB_WORKSPACE/__build/build" "$GITHUB_WORKSPACE/"
-# else
-#     echo "WARNING: Using build/vscdc from $GITHUB_REF instead of main."
-# fi
-# rm -rf node_modules
-# yarn install
-
-# Run test build
+# Build the image
 chmod +x build/devcontainers-cli.tgz
 npx --yes build/devcontainers-cli.tgz build --workspace-folder src/${DEFINITION} --image-name vsc-${DEFINITION}
 
 docker images
-# chmod +x build/vscdc
-# build/vscdc push  ${DEFINITION} \
-#     --no-push \
-#     --release dev \
-#     --github-repo "microsoft/vscode-dev-containers" \
-#     --registry "mcr.microsoft.com" \
-#     --registry-path "vscode/devcontainers" \
-#     --stub-registry "mcr.microsoft.com" \
-#     --stub-registry-path "vscode/devcontainers"
