799d3bd8dcacadd4eb11870932c1e639e650785b
Samruddhi Khandale <skhandale@microsoft.com>
2022-05-23 23:17:30 +0000

    updating build.sh

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -4,13 +4,9 @@ DEFINITION="$1"
 set -e
 
 export DOCKER_BUILDKIT=1
-echo "Building image"
-dir=$(pwd)
-echo $pwd
-ls -l
-# Build the image
+echo "Building image - ${DEFINITION}"
 chmod +x build/devcontainers-cli.tgz
-ls -l
-npx --yes build/devcontainers-cli.tgz build --workspace-folder src/${DEFINITION} --image-name vsc-${DEFINITION}
+cd build
+npx --yes devcontainers-cli.tgz build --workspace-folder "../src/${DEFINITION}/" --image-name vsc-${DEFINITION}
 
 docker images
