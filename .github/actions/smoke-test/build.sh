e7cf08577d3f8f2e74e8c9b9534bdec0bcb626df
Samruddhi Khandale <skhandale@microsoft.com>
2022-05-23 22:47:49 +0000

    add orignal contents

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -5,6 +5,13 @@ set -e
 
 export DOCKER_BUILDKIT=1
 
+# Symlink build scripts from main to improve security when testing PRs
+if [ -d "$GITHUB_WORKSPACE/__build/build" ]; then
+    cp -r "$GITHUB_WORKSPACE/__build/build" "$GITHUB_WORKSPACE/"
+else
+    echo "WARNING: Using build/vscdc from $GITHUB_REF instead of main."
+fi
+
 # Build the image
 chmod +x build/devcontainers-cli.tgz
 npx --yes build/devcontainers-cli.tgz build --workspace-folder src/${DEFINITION} --image-name vsc-${DEFINITION}
