d9ee49233f2aa5b50d81a147058ff9dc334852cc
Samruddhi Khandale <skhandale@microsoft.com>
2022-06-15 21:39:34 +0000

    test using vscdc

--- .github/actions/smoke-test/test.sh
+++ .github/actions/smoke-test/test.sh
@@ -1,19 +1,34 @@
 #/bin/bash
 DEFINITION="$1"
-USERNAME="$2"
+IMAGE="$2"
+USERNAME="$3"
 
 export DOCKER_BUILDKIT=1
 set -e
 
-# Run actual test
-echo "(*) Running test..."
-cd build
-chmod +x devcontainers-cli-0.3.0-1.tgz
-
-docker ps -a
 docker images
 
-npx --yes devcontainers-cli-0.3.0-1.tgz exec --workspace-folder $(pwd)/../src/$DEFINITION /bin/sh -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && if [ "$(id -u)" = "0" ]; then chmod +x test.sh; else sudo chmod +x test.sh; fi && ./test.sh; else ls -a; fi'
+# Start container
+echo "(*) Starting container..."
+container_name="vscdc-test-container-$DEFINITION"
+docker run -d --name ${container_name} --rm --init --privileged -v "$(pwd)/src/${DEFINITION}:/workspace" ${IMAGE}-uid /bin/sh -c 'while sleep 1000; do :; done'
+
+# Run actual test
+echo "(*) Running test..."
+docker exec -u "${USERNAME}" ${container_name} /bin/sh -c  '\
+    set -e \
+    && cd /workspace \
+    && if [ -f "test-project/test.sh" ]; then \
+    cd test-project \
+    && if [ "$(id -u)" = "0" ]; then \
+        chmod +x test.sh; \
+    else \
+        sudo chmod +x test.sh; \
+    fi \
+    && ./test.sh; \
+    else \
+    ls -a; 
+    fi'
 
 # Clean up
-docker stop $(docker container ls -q)
+docker rm -f ${container_name}
