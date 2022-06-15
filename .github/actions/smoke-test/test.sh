36d8f503cb82c571b98887afd686a58fcc8857f0
Samruddhi Khandale <skhandale@microsoft.com>
2022-06-15 22:43:25 +0000

    uid gid changes

--- .github/actions/smoke-test/test.sh
+++ .github/actions/smoke-test/test.sh
@@ -10,10 +10,25 @@ set -e
 echo "(*) Listing docker images..."
 docker images
 
+
+# Update UID/GID for user in container - Actions uses different UID/GID than container
+# which causes bind mounts to be read only and cause certain write tests to fail
+# The dev container CLI handles this automatically but we're not using it.
+local_uid=$(id -u)
+local_gid=$(id -g)
+echo "(*) Updating container user UID/GID..."
+echo -e "FROM ${IMAGE}\n \
+    RUN export sudo_cmd="" \
+    && if [ "$(id -u)" != "0" ]; then export sudo_cmd=sudo; fi \
+    && \${sudo_cmd} groupmod -g ${local_gid} ${USERNAME} \
+    && \${sudo_cmd} usermod -u ${local_uid} -g ${local_gid} ${USERNAME}" > uid.Dockerfile
+cat uid.Dockerfile
+docker build -t ${IMAGE}-uid -f uid.Dockerfile .
+
 # Start container
 echo "(*) Starting container..."
 container_name="vscdc-test-container-$DEFINITION"
-docker run -d --name ${container_name} --rm --init --privileged -v "$(pwd)/src/${DEFINITION}:/workspace" ${IMAGE} /bin/sh -c 'while sleep 1000; do :; done'
+docker run -d --name ${container_name} --rm --init --privileged -v "$(pwd)/src/${DEFINITION}:/workspace" ${IMAGE}-uid /bin/sh -c 'while sleep 1000; do :; done'
 
 # Run actual test
 echo "(*) Running test..."
