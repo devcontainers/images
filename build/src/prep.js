cabc480c2a15b756d307456a072619ebaf6f648d
Samruddhi Khandale <skhandale@microsoft.com>
2022-06-21 23:14:17 +0000

    remove build arg from .devcontainer.json

--- build/src/prep.js
+++ build/src/prep.js
@@ -224,7 +224,7 @@ function addLabels(prepResult) {
 }
 
 function replaceVariantArg(prepResult) {
-    const variantArg = `ARG VARIANT=${prepResult.meta.variant}\n`;
+    const variantArg = `ARG VARIANT="${prepResult.meta.variant}"\n`;
 
     prepResult.devContainerDockerfileModified = (prepResult.devContainerDockerfileModified).replace(new RegExp(".*ARG VARIANT=.*"), variantArg);
     return prepResult.devContainerDockerfileModified;
