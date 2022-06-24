2500e7e36ac641e99ce07b47380522d401e1b462
Samruddhi Khandale <skhandale@microsoft.com>
2022-06-24 17:54:37 +0000

    fix builders bug

--- build/src/push.js
+++ build/src/push.js
@@ -225,7 +225,7 @@ async function isImageAlreadyPublished(registryName, repositoryName, tagName) {
     return false;
 }
 
-async function createOrUseBuilder(builders) {
+async function createOrUseBuilder() {
     const builders = await asyncUtils.exec('docker buildx ls');
     if(builders.indexOf(builderName) < 0) {
         await asyncUtils.spawn('docker', ['buildx', 'create', '--use', '--name', builderName]);
