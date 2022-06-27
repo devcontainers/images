6057971125694d6c378b790fdbd4d87821ffa46c
Samruddhi Khandale <skhandale@microsoft.com>
2022-06-27 18:16:23 +0000

    cleanup code

--- src/codespaces/local-features/jekyll/install.sh
+++ src/codespaces/local-features/jekyll/install.sh
@@ -14,45 +14,6 @@ USERNAME=${2:-"codespace"}
 
 set -e
 
-# If in automatic mode, determine if a user already exists, if not use codespace
-if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
-    USERNAME=""
-    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-    for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
-        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
-            USERNAME=${CURRENT_USER}
-            break
-        fi
-    done
-    if [ "${USERNAME}" = "" ]; then
-        USERNAME=codespace
-    fi
-elif [ "${USERNAME}" = "none" ]; then
-    USERNAME=root
-    USER_UID=0
-    USER_GID=0
-fi
-
-# Use sudo to run as non-root user is not already running
-sudo_if() {
-    COMMAND="$*"
-    if [ "$(id -u)" -eq 0 ] && [ "$USERNAME" != "root" ]; then
-        su - "$USERNAME" -c "$COMMAND"
-    else
-        "$COMMAND"
-    fi
-}
-
-updaterc() {
-    echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
-    if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
-        echo -e "$1" >> /etc/bash.bashrc
-    fi
-    if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
-        echo -e "$1" >> /etc/zsh/zshrc
-    fi
-}
-
 # If we don't yet have Ruby installed, exit.
 if ! /usr/local/rvm/rubies/default/bin/ruby --version > /dev/null ; then
     echo "You need to install Ruby before installing Jekyll."
