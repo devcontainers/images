9b4517b936e3d80625333c9db4850a51662b34cf
Samruddhi Khandale <skhandale@microsoft.com>
2022-08-04 20:45:02 +0000

    test with cli v0.8.1

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -4,8 +4,8 @@ IMAGE="$1"
 set -e
 
 export DOCKER_BUILDKIT=1
-echo "(*) Installing @devcontainer/cli@0.8.0"
-npm install -g @devcontainers/cli@0.8.0
+echo "(*) Installing @devcontainer/cli@0.8.1"
+npm install -g @devcontainers/cli@0.8.1
 
 echo "(*) Building image - ${IMAGE}"
 devcontainer up --workspace-folder "src/${IMAGE}/"
