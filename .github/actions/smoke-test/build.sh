facd53cba045d493f7ead4b420a88040d445e557
Samruddhi Khandale <skhandale@microsoft.com>
2022-08-04 16:47:09 +0000

    test 0.9.1

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -5,7 +5,11 @@ set -e
 
 export DOCKER_BUILDKIT=1
 echo "(*) Installing @devcontainer/cli"
-npm install -g @devcontainers/cli
+cd build 
+chmod +x devcontainers-cli-0.9.1.tgz
+
+# npm install -g @devcontainers/cli
 
 echo "(*) Building image - ${IMAGE}"
-devcontainer up --workspace-folder "src/${IMAGE}/" --id-label name="${IMAGE}"
+npx --yes devcontainers-cli-0.9.1.tgz up --workspace-folder ../src/${IMAGE}  --id-label name="${IMAGE}"
+# devcontainer up --workspace-folder "src/${IMAGE}/" --id-label name="${IMAGE}"
--- .github/actions/smoke-test/test.sh
+++ .github/actions/smoke-test/test.sh
@@ -6,8 +6,15 @@ set -e
 
 # Run actual test
 echo "(*) Running test..."
+cd build
+chmod +x devcontainers-cli-0.9.1.tgz
 
-devcontainer exec --workspace-folder $(pwd)/src/$IMAGE  --id-label "name=${IMAGE}" /bin/sh -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && if [ "$(id -u)" = "0" ]; then chmod +x test.sh; else sudo chmod +x test.sh; fi && ./test.sh; else ls -a; fi'
+npx --yes devcontainers-cli-0.9.1.tgz exec --workspace-folder $(pwd)/../src/$IMAGE --id-label "name=${IMAGE}" /bin/sh -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && if [ "$(id -u)" = "0" ]; then chmod +x test.sh; else sudo chmod +x test.sh; fi && ./test.sh; else ls -a; fi'
 
 # Clean up
 docker rm -f $(docker container ls -f "label=name=${IMAGE}" -q)
+
+# devcontainer exec --workspace-folder $(pwd)/src/$IMAGE  --id-label "name=${IMAGE}" /bin/sh -c 'set -e && if [ -f "test-project/test.sh" ]; then cd test-project && if [ "$(id -u)" = "0" ]; then chmod +x test.sh; else sudo chmod +x test.sh; fi && ./test.sh; else ls -a; fi'
+
+# Clean up
+# docker rm -f $(docker container ls -f "label=name=${IMAGE}" -q)
