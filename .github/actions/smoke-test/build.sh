f747d1ac86597e57db1fc7547b60b3fa7f07c3f5
Samruddhi Khandale <skhandale@microsoft.com>
2022-08-04 18:57:18 +0000

    test bug fix

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -6,10 +6,10 @@ set -e
 export DOCKER_BUILDKIT=1
 echo "(*) Installing @devcontainer/cli"
 cd build 
-chmod +x devcontainers-cli-0.9.1.tgz
+chmod +x devcontainers-cli-0.9.1-1.tgz
 
 # npm install -g @devcontainers/cli
 
 echo "(*) Building image - ${IMAGE}"
-npx --yes devcontainers-cli-0.9.1.tgz up --workspace-folder ../src/${IMAGE}  --id-label name="${IMAGE}"
+npx --yes devcontainers-cli-0.9.1-1.tgz up --workspace-folder ../src/${IMAGE}  --id-label name="${IMAGE}"
 # devcontainer up --workspace-folder "src/${IMAGE}/" --id-label name="${IMAGE}"
