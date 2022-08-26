e16537db11f93faa76f11b83334e958e999c4938
Samruddhi Khandale <skhandale@microsoft.com>
2022-08-26 21:56:00 +0000

    add java-8

--- src/java-8/test-project/test.sh
+++ src/java-8/test-project/test.sh
@@ -8,22 +8,23 @@ checkCommon
 
 # Prep
 echo -e "\nGetting Maven wrapper..."
-curl -sSL https://github.com/takari/maven-wrapper/archive/maven-wrapper-0.5.5.tar.gz| tar -xzf - 
+curl -sSL https://github.com/takari/maven-wrapper/archive/maven-wrapper-0.5.5.tar.gz| tar -xzf -
 mv maven-wrapper-maven-wrapper-0.5.5/mvnw mvnw
 mv maven-wrapper-maven-wrapper-0.5.5/.mvn .mvn
 rm -rf mv maven-wrapper-maven-wrapper-0.5.5
 
 # Image specific tests
+checkExtension "vscjava.vscode-java-pack"
 check "java" java -version
 check "build-and-test-jar" ./mvnw -q package
 check "test-project" java -jar target/my-app-1.0-SNAPSHOT.jar
-check "nvm" bash -c ". /usr/local/share/nvm/nvm.sh && nvm install 10"
-check "nvm-node" bash -c ". /usr/local/share/nvm/nvm.sh && node --version"
-check "yarn" bash -c ". /usr/local/share/nvm/nvm.sh && yarn --version"
 
 # Clean up
 rm -f mvnw
 rm -rf .mvn
+
+"checki path"
+which java
 
 # Report result
 reportResults
