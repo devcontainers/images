31f9fe3f612854e9aa635f521b2f5ee9af4b4815
Samruddhi Khandale <skhandale@microsoft.com>
2022-10-18 21:55:15 -0700

    test

--- src/dotnet/test-project/test.sh
+++ src/dotnet/test-project/test.sh
@@ -16,12 +16,15 @@ check "nvm-node" bash -c ". /usr/local/share/nvm/nvm.sh && node --version"
 check "yarn" bash -c ". /usr/local/share/nvm/nvm.sh && yarn --version"
 check "git" git --version
 
-git_version_satisfied=false
+git_version_satisfied="false"
 if (echo a version 2.38.1; git --version) | sort -Vk3 | tail -1 | grep -q git; then
     git_version_satisfied=true
 fi
 
+echo $git_version_satisfied
 check "gitSatisfied" echo $git_version_satisfied | grep "true"
+check "gits" echo $git_version_satisfied | grep "true"
+check "gitsy" if [${git_version_satisfied} = "true"]; then echo "satisfied"; fi
 
 # Report result
 reportResults
