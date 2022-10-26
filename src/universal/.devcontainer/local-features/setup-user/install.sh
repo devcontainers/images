bf037e211a378f27f33d2718988007fee479589f
Samruddhi Khandale <skhandale@microsoft.com>
2022-10-26 22:07:52 +0000

    nit

--- src/universal/.devcontainer/local-features/setup-user/install.sh
+++ src/universal/.devcontainer/local-features/setup-user/install.sh
@@ -26,7 +26,7 @@ mkdir -p /opt/oryx && echo "vso-focal" > /opt/oryx/.imagetype
 echo "DEBIAN|${DEBIAN_FLAVOR}" | tr '[a-z]' '[A-Z]' > /opt/oryx/.ostype
 
 # Oryx expects the tool to be installed at `/opt/oryx` and looks for relevant files in there.
-ln -snf /usr/local/oryx /opt/oryx
+ln -snf /usr/local/oryx/* /opt/oryx
 
 # For the universal image, oryx build tool installs the detected platforms in /home/codespace/*. Hence, linking current platforms to the /home/codespace/ path and adding it to the PATH.
 # This ensures that whatever platfornm versions oryx detects and installs are set as root.
