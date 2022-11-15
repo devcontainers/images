a6ff0340f30c9bee62e04c9f77bdc1f6f36a0337
Samruddhi Khandale <skhandale@microsoft.com>
2022-11-15 21:56:44 +0000

    nit

--- src/universal/test-project/test.sh
+++ src/universal/test-project/test.sh
@@ -137,13 +137,12 @@ pythonVersion=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
 echo "pythonVersion ${pythonVersion}"
 pythonSite=`python -m site --user-site`
 echo "pythonSite ${pythonSite}"
-echo "cmd oryx build --property python_version="${pythonVersion}" --property packagedir="${pythonSite}" --debug --log-file oryx-logs ./sample/python"
+echo "cmd oryx build --property python_version="${pythonVersion}" --property packagedir="${pythonSite}" --debug --log-file oryx-logs.log ./sample/python"
 
+
+check "oryx-build-python" oryx build --property python_version="${pythonVersion}" --property packagedir="${pythonSite}" ./sample/python
 echo "Reading logs"
 cat oryx-logs.log
-
-ls -la
-check "oryx-build-python" oryx build --property python_version="${pythonVersion}" --property packagedir="${pythonSite}" ./sample/python
 check "oryx-build-python-installed" python3 -m pip list | grep mpmath
 check "oryx-build-python-result" python3 ./sample/python/src/solve.py
 
