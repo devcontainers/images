09243ee319f36b271f59dc6071d71eb92b136928
Samruddhi Khandale <skhandale@microsoft.com>
2022-12-16 23:24:37 +0000

    Universal: Update "commons-io" due to CVE-2021-29425

--- src/universal/.devcontainer/local-features/setup-user/install.sh
+++ src/universal/.devcontainer/local-features/setup-user/install.sh
@@ -38,6 +38,13 @@ curl -sSL https://github.com/jhy/jsoup/archive/refs/tags/jsoup-1.15.3.tar.gz | t
 jar cf ${GRADLE_PATH}/jsoup-1.15.3.jar /tmp/jsoup-jsoup-1.15.3
 rm -rf /tmp/jsoup-jsoup-1.15.3
 
+# Temporary: Due to https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-29425
+MAVEN_PATH=$(cd /usr/local/sdkman/candidates/maven/3*/lib/ && pwd)
+rm -f ${MAVEN_PATH}/commons-io-*
+curl -sSL https://github.com/apache/commons-io/archive/refs/tags/commons-io-2.11.0-RC1.tar.gz | tar -xzC /tmp 2>&1
+jar cf ${MAVEN_PATH}/commons-io-2.11.jar /tmp/commons-io-commons-io-2.11.0-RC1
+rm -rf /tmp/commons-io-commons-io-2.11.0-RC1
+
 # Temporary: Upgrade NPM packages due to mentioned CVEs.
 # decode-uri-component: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2022-38900
 # ansi-regex: https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2021-3807
