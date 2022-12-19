91e9819643185f51a4579f9f7bf6d27c962e35b7
Samruddhi Khandale <skhandale@microsoft.com>
2022-12-19 19:39:34 +0000

    Prep Release continued: Bump image versions

--- src/base-debian/test-project/test.sh
+++ src/base-debian/test-project/test.sh
@@ -15,5 +15,9 @@ fi
 
 check "git version satisfies requirement" echo $git_version_satisfied | grep "true"
 
+cd /tmp && git clone https://github.com/devcontainers/feature-starter.git
+cd feature-starter
+check "perl" bash -c "git -c grep.patternType=perl grep -q 'a.+b'"
+
 # Report result
 reportResults
