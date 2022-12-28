2e31480557eec0bc11cceca75ad4a8f39187b36d
Samruddhi Khandale <skhandale@microsoft.com>
2022-12-28 21:22:24 +0000

    update

--- src/javascript-node/test-project/test.sh
+++ src/javascript-node/test-project/test.sh
@@ -8,13 +8,6 @@ checkCommon
 
 # Image specific tests
 check "node" node --version
-
-# Patch tests
-cd /usr/local/lib/node_modules/npm/node_modules/string-width/
-
-ansiVersion=$(npm ls --depth 1 --json | jq -r '.dependencies."ansi-regex".version')
-check-version-ge "ansi-regex" "${ansiVersion}" "6.0.1"
-
 sudo rm -f yarn.lock
 check "yarn" yarn install
 sudo rm -f package-lock.json
@@ -28,6 +21,7 @@ sudo rm -rf node_modules
 
 git_version=$(git --version)
 check-version-ge "git-requirement" "${git_version}" "git version 2.38.1"
+
 
 # Report result
 reportResults
