22193ca5b2a1abd0f9a735fd479a5ec01ce0d7bd
Samruddhi Khandale <skhandale@microsoft.com>
2023-01-13 10:56:29 -0800

    Node - 19 & 18

--- src/universal/test-project/test.sh
+++ src/universal/test-project/test.sh
@@ -82,8 +82,8 @@ check "nvs" bash -c ". /usr/local/nvs/nvs.sh && nvs --version"
 check "yarn" yarn --version
 check "npm" npm --version
 count=$(ls /usr/local/share/nvm/versions/node | wc -l)
-expectedCount=1
-checkVersionCount "one version of node is present" $count $expectedCount
+expectedCount=2
+checkVersionCount "two versions of node are present" $count $expectedCount
 echo $(echo "node versions" && ls -a /usr/local/share/nvm/versions/node)
 
 # PHP
@@ -129,7 +129,7 @@ check "run-puppeteer" node puppeteer.js
 check "oryx" oryx --version
 
 # Ensures nvm works in a Node Project
-check "default-node-version" bash -c "node --version | grep 18."
+check "default-node-version" bash -c "node --version | grep 19."
 check "default-node-location" bash -c "which node | grep /home/codespace/nvm/current/bin"
 check "oryx-build-node-projectr" bash -c "oryx build ./sample/node"
 check "oryx-configured-current-node-version" bash -c "ls -la /home/codespace/nvm/current | grep /opt/nodejs"
