3e4908e6e922adb117025dd977709f23a5e7d965
Alexander Smolyakov <v-asmoliakov@microsoft.com>
2023-06-05 17:41:25 +0400

    Remove temp fix for `setuptools`

--- src/universal/.devcontainer/local-features/setup-user/install.sh
+++ src/universal/.devcontainer/local-features/setup-user/install.sh
@@ -43,8 +43,6 @@ sudo_if() {
 
 # Temporary: Upgrade python packages due to https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2022-40897
 # They are installed by the base image (python) which does not have the patch.
-sudo_if /usr/local/python/current/bin/python -m pip uninstall --yes setuptools
-sudo_if /usr/local/python/current/bin/python -m pip install --user --upgrade --no-cache-dir setuptools
 sudo_if /usr/local/python/3.9.*/bin/python -m pip uninstall --yes setuptools
 sudo_if /usr/local/python/3.9.*/bin/python -m pip install --user --upgrade --no-cache-dir setuptools
 
