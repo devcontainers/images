93c29b1458e7492263d7c710d4fcf001a76222c5
Alexander Smolyakov <v-asmoliakov@microsoft.com>
2023-06-20 12:10:23 +0400

    [miniconda] Patch Python due to GHSA-5cpq-8wj7-hf2v
    - Update Dockerfile to install the latest versions of pyopenssl and cryptography;
    - Added tests to verify minimum version for pyopenssl and cryptography packages;
    - Added tests to verify minimum package version via `conda list` command.

--- src/miniconda/test-project/test.sh
+++ src/miniconda/test-project/test.sh
@@ -18,10 +18,17 @@ check "gitconfig-contains-name" sh -c "cat /etc/gitconfig | grep 'name = devcont
 
 check "usr-local-etc-config-does-not-exist" test ! -f "/usr/local/etc/gitconfig"
 
-checkPythonPackageVersion "cryptography" "38.0.3"
+checkPythonPackageVersion "cryptography" "41.0.0"
+checkPythonPackageVersion "pyopenssl" "23.2.0"
 checkPythonPackageVersion "setuptools" "65.5.1"
 checkPythonPackageVersion "wheel" "0.38.1"
 checkPythonPackageVersion "requests" "2.31.0"
+
+checkCondaPackageVersion "cryptography" "41.0.0"
+checkCondaPackageVersion "pyopenssl" "23.2.0"
+checkCondaPackageVersion "setuptools" "65.5.1"
+checkCondaPackageVersion "wheel" "0.38.1"
+checkCondaPackageVersion "requests" "2.31.0"
 
 check "conda-update-conda" bash -c "conda update -y conda"
 check "conda-install" bash -c "conda install -c conda-forge --yes tensorflow"
