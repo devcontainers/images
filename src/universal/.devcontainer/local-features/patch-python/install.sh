067f004262e122890a6b34b935191b6c1716e268
Alexander Smolyakov <v-asmoliakov@microsoft.com>
2023-08-10 19:30:07 +0400

    Update `patch-python` feature
    - Remove `setuptools` patch for Python 3.10 since not required anymore;
    - Lock versions for packages;

--- src/universal/.devcontainer/local-features/patch-python/install.sh
+++ src/universal/.devcontainer/local-features/patch-python/install.sh
@@ -41,8 +41,7 @@ update_package() {
 # They are installed by the base image (python) which does not have the patch.
 
 # https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2022-40897
-update_package /usr/local/python/3.9.*/bin/python setuptools
-update_package /usr/local/python/3.10.*/bin/python setuptools
+update_package /usr/local/python/3.9.*/bin/python setuptools==65.5.1
 
 # https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2023-32681
-update_package /usr/local/python/3.10.*/bin/python requests
+update_package /usr/local/python/3.10.*/bin/python requests==2.31.0
