f1bc3f8ca059ac50496373f642a391508b1acac8
Samruddhi Khandale <skhandale@microsoft.com>
2023-09-21 18:46:40 +0000

    remove cgroup nesting

--- src/universal/.devcontainer/local-features/docker-in-docker/install.sh
+++ src/universal/.devcontainer/local-features/docker-in-docker/install.sh
@@ -384,38 +384,17 @@ dockerd_start="AZURE_DNS_AUTO_DETECTION=${AZURE_DNS_AUTO_DETECTION} DOCKER_DEFAU
         mount -t tmpfs none /tmp
     fi
 
-    set_cgroup_nesting()
-    {
-        # cgroup v2: enable nesting
-        if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
-            # move the processes from the root group to the /init group,
-            # otherwise writing subtree_control fails with EBUSY.
-            # An error during moving non-existent process (i.e., "cat") is ignored.
-            mkdir -p /sys/fs/cgroup/init
-            xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
-            # enable controllers
-            sed -e 's/ / +/g' -e 's/^/+/' < /sys/fs/cgroup/cgroup.controllers \
-                > /sys/fs/cgroup/cgroup.subtree_control
-        fi
-    }
-
-    # Set cgroup nesting, retrying if necessary
-    retry_cgroup_nesting=0
-
-    until [ "${retry_cgroup_nesting}" -eq "5" ];
-    do
-        set +e
-            set_cgroup_nesting
-
-            if [ $? -ne 0 ]; then
-                echo "(*) cgroup v2: Failed to enable nesting, retrying..."
-            else
-                break
-            fi
-
-            retry_cgroup_nesting=`expr $retry_cgroup_nesting + 1`
-        set -e
-    done 
+    # cgroup v2: enable nesting
+    if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
+        # move the processes from the root group to the /init group,
+        # otherwise writing subtree_control fails with EBUSY.
+        # An error during moving non-existent process (i.e., "cat") is ignored.
+        mkdir -p /sys/fs/cgroup/init
+        xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
+        # enable controllers
+        sed -e 's/ / +/g' -e 's/^/+/' < /sys/fs/cgroup/cgroup.controllers \
+            > /sys/fs/cgroup/cgroup.subtree_control
+    fi
 
     # -- End: dind wrapper script --
 
