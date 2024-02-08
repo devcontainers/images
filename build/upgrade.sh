fdfa6b7579d832c8088fd7fbfcb4f14f63b0a34a
Samruddhi Khandale <skhandale@microsoft.com>
2024-02-08 19:05:33 +0000

    remove stale file

--- build/upgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for dir in ./src/*/ ; do
-    if [ -d "$dir" ]; then
-        echo "Upgrading devcontainer lockfiles for '$dir'"
-        devcontainer upgrade --workspace-folder "$dir"
-    fi
-done
