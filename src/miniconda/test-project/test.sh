9bb5c4f0540a9a54b0d4ea4151b737c7af2f8aec
gauravsaini04 <147703805+gauravsaini04@users.noreply.github.com>
2024-02-17 04:54:35 +0530

    [MIniconda] - Cryptography - Patch security vulnerability - GHSA-3ww4-gg4f-jr7f (#965)
    * [MIniconda] - Cryptography - Patch security vulnerability - GHSA-3ww4-gg4f-jr7f
    
    * changes according to comments
    
    * install pyopenssl v24.0.0
    
    * Added tests to show that pip works correctly

--- src/miniconda/test-project/test.sh
+++ src/miniconda/test-project/test.sh
@@ -18,12 +18,12 @@ check "gitconfig-contains-name" sh -c "cat /etc/gitconfig | grep 'name = devcont
 
 check "usr-local-etc-config-does-not-exist" test ! -f "/usr/local/etc/gitconfig"
 
-checkPythonPackageVersion "cryptography" "41.0.7"
+checkPythonPackageVersion "cryptography" "42.0.2"
 checkPythonPackageVersion "setuptools" "65.5.1"
 checkPythonPackageVersion "wheel" "0.38.1"
 
-checkCondaPackageVersion "cryptography" "41.0.7"
-checkCondaPackageVersion "pyopenssl" "23.2.0"
+checkCondaPackageVersion "cryptography" "42.0.2"
+checkCondaPackageVersion "pyopenssl" "24.0.0"
 checkCondaPackageVersion "setuptools" "65.5.1"
 checkCondaPackageVersion "wheel" "0.38.1"
 checkCondaPackageVersion "requests" "2.31.0"
@@ -32,6 +32,8 @@ checkCondaPackageVersion "urllib3" "1.26.17"
 check "conda-update-conda" bash -c "conda update -y conda"
 check "conda-install-tensorflow" bash -c "conda create --name test-env -c conda-forge --yes tensorflow"
 check "conda-install-pytorch" bash -c "conda create --name test-env -c conda-forge --yes pytorch"
+
+checkPipWorkingCorrectly
 
 # Report result
 reportResults
