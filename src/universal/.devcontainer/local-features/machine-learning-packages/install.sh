dba42b0eb5fed776e19fa0dc9f6c7ca0807ea56d
gauravsaini04 <147703805+gauravsaini04@users.noreply.github.com>
2024-04-07 21:39:46 +0000

    [universal] - reduce size - changed acc. to comments

--- src/universal/.devcontainer/local-features/machine-learning-packages/install.sh
+++ src/universal/.devcontainer/local-features/machine-learning-packages/install.sh
@@ -29,34 +29,12 @@ sudo_if() {
 
 export DEBIAN_FRONTEND=noninteractive
 
-install_torch_check_gpu() {
-    if ! command -v lspci &> /dev/null; then
-        echo "lspci not found. Attempting to install..."
-        apt-get update && apt-get install -y pciutils
-    fi
-    # Detect GPU
-    set +e
-    GPU=$(lspci | grep -i NVIDIA)
-    set -e
-    if [ -n "$GPU" ]; then
-        echo "GPU Detected. Installing Torch with GPU support."
-        sudo_if /usr/local/python/current/bin/python -m pip install --user --upgrade --no-cache-dir torch
-    else
-        echo "No GPU Detected. Installing Torch with CPU support."
-        sudo_if /usr/local/python/current/bin/python -m pip install --user --upgrade --no-cache-dir torch -f https://download.pytorch.org/whl/cpu/torch_stable.html
-    fi
-}
-
 install_python_package() {
     PACKAGE=${1:-""}
 
     sudo_if /usr/local/python/current/bin/python -m pip uninstall --yes $PACKAGE
     echo "Installing $PACKAGE..."
-    if [[ $PACKAGE == "torch" ]]; then
-        install_torch_check_gpu
-    else 
-        sudo_if /usr/local/python/current/bin/python -m pip install --user --upgrade --no-cache-dir $PACKAGE
-    fi
+    sudo_if /usr/local/python/current/bin/python -m pip install --user --upgrade --no-cache-dir $PACKAGE
 }
 
 if [[ "$(python --version)" != "" ]] && [[ "$(pip --version)" != "" ]]; then
@@ -66,7 +44,6 @@ if [[ "$(python --version)" != "" ]] && [[ "$(pip --version)" != "" ]]; then
     install_python_package "matplotlib"
     install_python_package "seaborn"
     install_python_package "scikit-learn"
-    install_python_package "torch"
     install_python_package "requests"
     install_python_package "plotly"
 else
