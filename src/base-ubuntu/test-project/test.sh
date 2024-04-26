40256686e5aee26a4b50209d0d1f7a032bf55d3b
Samruddhi Khandale <skhandale@microsoft.com>
2024-04-26 17:46:45 +0000

    test

--- src/base-ubuntu/test-project/test.sh
+++ src/base-ubuntu/test-project/test.sh
@@ -14,13 +14,18 @@ check "git" git --version
 git_version=$(git --version)
 check-version-ge "git-requirement" "${git_version}" "git version 2.40.1"
 
+id -g vscode
+cat /etc/passwd | grep vscode
+cat /etc/passwd | grep ubuntu
+
+check "uid" bash -c "id -u vscode | grep 1001" 
+check "gid" bash -c "id -g vscode | grep 1001"
+
+
 check "set-git-config-user-name" sh -c "sudo git config --system user.name devcontainers"
 check "gitconfig-file-location" sh -c "ls /etc/gitconfig"
 check "gitconfig-contains-name" sh -c "cat /etc/gitconfig | grep 'name = devcontainers'"
 check "usr-local-etc-config-does-not-exist" test ! -f "/usr/local/etc/gitconfig"
-
-check "uid" bash -c "id -u vscode | grep 1001" 
-check "gid" bash -c "id -g vscode | grep 1001"
 
 # Report result
 reportResults
