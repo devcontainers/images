a5fe28bee4dd323acc53d2d3702e6f2ca00d4182
Samruddhi Khandale <skhandale@microsoft.com>
2024-05-08 14:33:59 -0700

    Adds support for Cpp 24.04 and Typescript-node 22 (#1055)

---
+++ src/typescript-node/test-project/eslint.config.js
@@ -0,0 +1,11 @@
+module.exports={
+    "rules": {
+        "no-console": 0,
+        "eqeqeq":"warn",
+        "no-cond-assign": 0,
+        "no-unused-vars": 1,
+        "no-extra-semi": "warn",
+        "semi": "warn"
+    },
+    ignorePatterns: ["**/node_modules/**"]
+};
