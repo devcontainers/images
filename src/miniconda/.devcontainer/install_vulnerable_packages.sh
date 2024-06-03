34e63ee4eaeb170990d9568b08a649bab406a245
gauravsaini04 <147703805+gauravsaini04@users.noreply.github.com>
2024-06-03 04:58:15 +0000

    [Miniconda] - Auto install vuln packages

---
+++ src/miniconda/.devcontainer/install_vulnerable_packages.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+vulnerable_packages=( "idna=3.7" )
+
+# Define the number of rows (based on the length of vulnerable_packages)
+rows=${#vulnerable_packages[@]}
+
+# Define the number of columns
+cols=2
+
+# Define the 2D array
+declare -A packages_array
+
+# Fill the 2D array
+for ((i=0; i<rows; i++)); do
+    # Split each element of vulnerable_packages by the '=' sign
+    IFS='=' read -ra parts <<< "${vulnerable_packages[$i]}"
+    # Assign the parts to the 2D array
+    packages_array[$i,0]=${parts[0]}
+    packages_array[$i,1]=${parts[1]}
+done
+
+for ((i=0; i<rows; i++)); do
+    CURRENT_VERSION=$(pip show "${packages_array[$i,0]}" | grep '^Version:' | awk '{print $2}')
+    if [[ -z "$CURRENT_VERSION" ]]; then
+        echo "No version for ${packages_array[$i,0]} found in upstream i.e. base image."
+        CURRENT_VERSION="0"
+    fi
+    REQUIRED_VERSION="${packages_array[$i,1]}"
+    GREATER_VERSION_A=$((echo ${REQUIRED_VERSION}; echo ${CURRENT_VERSION}) | sort -V | tail -1)
+    # Check if the required_version is greater than current_version
+    if [[ $CURRENT_VERSION != $GREATER_VERSION_A ]]; then
+        echo "${packages_array[$i,0]} version v${CURRENT_VERSION} installed by the base image is not greater or equal to the required: v${REQUIRED_VERSION}"
+        # Check whether conda channel has a greater or equal version available, so install from conda, otherwise use pip package manager
+        channel_name="anaconda"
+        CONDA_VERSION=$(conda search "${packages_array[$i,0]}" -c "$channel_name" | \
+            grep -E '^[[:alnum:]]' | \
+            awk '{print $2}' | \
+            sort -V | \
+            uniq | \
+            tail -n 2 | \
+            head -n 1)
+        if [[ -z "$CONDA_VERSION" ]]; then
+            echo "No version for ${packages_array[$i,0]} found in conda channel."
+            CONDA_VERSION="0"
+        fi
+        GREATER_VERSION_B=$((echo ${REQUIRED_VERSION}; echo ${CONDA_VERSION}) | sort -V | tail -1)
+        if [[ $CONDA_VERSION == $GREATER_VERSION_B ]]; then
+            echo -e "Conda Version: ${CONDA_VERSION} is greater than or equal to the required version: ${REQUIRED_VERSION}. \n";
+            echo "Installing ${packages_array[$i,0]} from source from conda channel for ${REQUIRED_VERSION}..."
+            conda install "${packages_array[$i,0]}==${CONDA_VERSION}"
+        elif [[ $REQUIRED_VERSION == $GREATER_VERSION_B ]]; then 
+            echo -e "Required version: ${REQUIRED_VERSION} is greater than the conda version: ${CONDA_VERSION}. \n";
+            echo "Installing ${packages_array[$i,0]} from source from pip package manager for ${REQUIRED_VERSION}..."
+            python3 -m pip install --upgrade "${packages_array[$i,0]}==${REQUIRED_VERSION}"
+        fi
+    fi
+done
