8ba44d7e93c68184b169a3320f301602bbd6159e
gauravsaini04 <147703805+gauravsaini04@users.noreply.github.com>
2024-06-18 08:22:12 +0000

    update files for pinning to required version

--- src/miniconda/.devcontainer/apply_security_patches.sh
+++ src/miniconda/.devcontainer/apply_security_patches.sh
@@ -2,7 +2,7 @@
 
 # define array of packages for pinning to the patched versions
 # vulnerable_packages=( "package1=version1" "package2=version2" "package3=version3" )
-vulnerable_packages=( "" )
+vulnerable_packages=( "tqdm=4.66.4" )
 
 # Define the number of rows (based on the length of vulnerable_packages)
 rows=${#vulnerable_packages[@]}
