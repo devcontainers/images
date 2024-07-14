593270dfc666a0d49175d22747ffef2d741d391c
gauravsaini04 <147703805+gauravsaini04@users.noreply.github.com>
2024-07-14 02:00:10 +0000

    more buster eol changes for cpp devcontainer

--- src/cpp/.devcontainer/base-scripts/install-vcpkg.sh
+++ src/cpp/.devcontainer/base-scripts/install-vcpkg.sh
@@ -10,17 +10,6 @@ USERNAME=${1:-"vscode"}
 
 . /etc/os-release
 
-# The buster pkg repo install cmake version < 3.15 which is required to run bootstrap-vcpkg.sh on ARM64
-VCPKG_UNSUPPORTED_ARM64_VERSION_CODENAMES="buster"
-
-if [ "$(dpkg --print-architecture)" = "arm64" ]; then
-    # Exit early if ARM64 OS does not have cmake version required to build Vcpkg
-    if [[ "${VCPKG_UNSUPPORTED_ARM64_VERSION_CODENAMES}" = *"${VERSION_CODENAME}"* ]]; then
-        echo "OS ${VERSION_CODENAME} ARM64 pkg repo installs cmake version < 3.15, which is required to build Vcpkg."
-        exit 0
-    fi
-fi
-
 # Add to bashrc/zshrc files for all users.
 updaterc()  {
     echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
