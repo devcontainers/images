65ff7b668c771a696790aea9796be3f5a1388552
Gaurav Saini <147703805+gauravsaini04@users.noreply.github.com>
2024-09-10 04:19:44 +0000

    changed test - check for ubuntu user after deletion added

--- src/base-ubuntu/test-project/test.sh
+++ src/base-ubuntu/test-project/test.sh
@@ -19,7 +19,16 @@ check "gitconfig-file-location" sh -c "ls /etc/gitconfig"
 check "gitconfig-contains-name" sh -c "cat /etc/gitconfig | grep 'name = devcontainers'"
 check "usr-local-etc-config-does-not-exist" test ! -f "/usr/local/etc/gitconfig"
 
-check "uid" bash -c "id -u vscode | grep 1001"
+check_ubuntu_user() {
+    if ! id -u ubuntu > /dev/null 2>&1; then
+        echo -e "✔️   User ubuntu does not exist."
+    else
+        echo -e "❌   User ubuntu exists."
+        exit 1;
+    fi
+}
+
+check "uid" "check_ubuntu_user"
 
 # Report result
 reportResults
