8f1a66e71f09d0f44962b4ff95adf65968ff1ce9
Gaurav Saini <147703805+gauravsaini04@users.noreply.github.com>
2024-09-26 07:18:11 +0000

    as required

--- src/dotnet/test-project/test.sh
+++ src/dotnet/test-project/test.sh
@@ -29,23 +29,5 @@ check "usr-local-etc-config-does-not-exist" test ! -f "/usr/local/etc/gitconfig"
 
 checkPackageVersion "pwsh" "7.4.4" "PowerShell"
 
-check_ubuntu_user() {
-    if ! id -u ubuntu > /dev/null 2>&1; then
-        echo -e "✔️   User ubuntu does not exist."
-    else
-        echo -e "❌   User ubuntu exists."
-        exit 1;
-    fi
-    echo -e "\n\nList of all users:";
-    cat /etc/passwd;
-}
-
-if grep -q 'VERSION_CODENAME=noble' /etc/os-release; then
-    echo -e "\nThe base image is ubuntu:noble. Checking user Ubuntu.."
-    check "uid" "check_ubuntu_user"
-else
-    echo -e "\nCannot check user Ubuntu. The base image is not ubuntu:noble."
-fi
-
 # Report result
 reportResults
