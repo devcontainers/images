b839a36724076caa248a4298b7b1bd21bde57bfb
Gaurav Saini <147703805+gauravsaini04@users.noreply.github.com>
2024-11-23 04:26:28 +0530

    [Miniconda] - remove stale patches ( when higher version is available from upstream ) (#1229)

--- src/miniconda/.devcontainer/apply_security_patches.sh
+++ src/miniconda/.devcontainer/apply_security_patches.sh
@@ -5,7 +5,7 @@
 
 # define array of packages for pinning to the patched versions
 # vulnerable_packages=( "package1=version1" "package2=version2" "package3=version3" )
-vulnerable_packages=( "tqdm=4.66.4" "requests=2.32.0" "urllib3=2.2.2" "certifi=2024.7.4" "cryptography=43.0.1" )
+vulnerable_packages=( "cryptography=43.0.1" )
 
 # Define the number of rows (based on the length of vulnerable_packages)
 rows=${#vulnerable_packages[@]}
