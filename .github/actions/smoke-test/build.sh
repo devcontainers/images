80c0d171b4315a02d75430b65e4e485d39763c21
Kaniska <kaniska244@github.com>
2025-02-24 22:29:33 +0530

    Revert "[universal] - Issue universal config change for non-root default code…"
    This reverts commit c930750d742f9dff898e37213dbcacfcf33e992f.

--- .github/actions/smoke-test/build.sh
+++ .github/actions/smoke-test/build.sh
@@ -7,10 +7,6 @@ export DOCKER_BUILDKIT=1
 echo "(*) Installing @devcontainer/cli"
 npm install -g @devcontainers/cli
 
-id_label="test-container=${IMAGE}"
-id_image="${IMAGE}-test-image"
 echo "(*) Building image - ${IMAGE}"
-devcontainer build --image-name ${id_image} --workspace-folder "src/${IMAGE}/"
-echo "(*) Starting container - ${IMAGE}"
+id_label="test-container=${IMAGE}"
 devcontainer up --id-label ${id_label} --workspace-folder "src/${IMAGE}/"
-
