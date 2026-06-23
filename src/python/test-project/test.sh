deb508ba618f1a8a1c95d9f0bb7df974c9d30ad7
Venkumahanti Subhankar <v-subhankar-infy@github.com>
2026-06-23 13:05:44 +0000

    [python] Update GitPython to 3.1.50

--- src/python/test-project/test.sh
+++ src/python/test-project/test.sh
@@ -49,7 +49,7 @@ check-version-ge "setuptools-requirement" "${setuptools_version}" "78.1.1"
 
 # https://github.com/advisories/GHSA-rpm5-65cw-6hj4
 gitpython_version=$(python -c "import git; print(git.__version__)")
-check-version-ge "gitpython-requirement" "${gitpython_version}" "3.1.47"
+check-version-ge "gitpython-requirement" "${gitpython_version}" "3.1.50"
 
 # GHSA-58pv-8j8x-9vj2: jaraco.context
 jaraco_context_version=$(python -c "from importlib.metadata import version; print(version('jaraco.context'))")
